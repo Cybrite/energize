7dfd12697196ba1cc9e2f7e1b6db285fd5755ffd
Harsh <harshtanishq2002@gmail.com>
2024-12-08 15:29:18 +0530

    feat: added live view count change

--- components/View.tsx
+++ components/View.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import Ping from "./Ping";
 import { VIEW_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+import { unstable_after as after } from "next/server";
 
 const View = async ({ id }: { id: string }) => {
+  const view = await client
+    .withConfig({ useCdn: false })
+    .fetch(VIEW_BY_ID_QUERY, { id });
 
-    const view = await client.withConfig({ useCdn: false }).fetch(VIEW_BY_ID_QUERY, { id });
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: view.views + 1 })
+        .commit(),
+  );
 
-    if (!view) return 0;
+  if (!view) return 0;
 
   return (
     <div className="view-container">
--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,9 @@ const nextConfig: NextConfig = {
     buildActivity: true,
     buildActivityPosition: "bottom-right",
   },
+  experimental: {
+    after: true,
+  }
 };
 
 export default nextConfig;
